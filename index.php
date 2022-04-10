2eea13d9c3ca73ea753d4a63e343b9c973176273
stefanom13 <stefanomancuso13@gmail.com>
2022-04-10 11:29:53 +0200

    creazione foglio html

--- index.php
+++ index.php
@@ -7,5 +7,19 @@ stampate a schermo i valori delle relative proprietà di ogni oggetto (potete cr
 
 <?php
     include_once __DIR__."/db.php";
-    
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  
+</body>
+</html>
