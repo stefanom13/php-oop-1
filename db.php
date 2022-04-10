db6f91228bb0e1c95fc7fc4f493a1d8d5c398ceb
stefanom13 <stefanomancuso13@gmail.com>
2022-04-10 11:22:49 +0200

    creazione db class movie

---
+++ db.php
@@ -0,0 +1,20 @@
+
+<?php
+class Movie{
+    public $title;
+    public $author;
+    public $year;
+    public $language;
+
+    function __construct(string $_title, string $_author, int $_year, string $_language)
+    {
+        $this->title=$_title;
+        $this->author=$_author;
+        $this->year=$_year;
+        $this->language=$_language;
+    }
+
+    public function getDescription(){
+        return $this->title.' di '.$this->author.'<br>Anno '.$this->year . ' <br> '.$this->language ;
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<!-- è definita una classe ‘Movie’
+    => all’interno della classe sono dichiarate delle variabili d’istanza
+    => all’interno della classe è definito un costruttore
+    => all’interno della classe è definito almeno un metodo
+    vengono istanziati almeno due oggetti ‘Movie’
+stampate a schermo i valori delle relative proprietà di ogni oggetto (potete creare un array con dentro le due istanze e fare un ciclo per stampare i dati) -->
+
